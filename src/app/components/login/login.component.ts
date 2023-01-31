f22474b71015758af5de6335e392f01ac9ddb690
Antonio Alvarado <tonny.xp@hotmail.com>
2023-01-30 21:08:05 -0700

    Implementación menús y formulario Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/interfaces/login';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { UtilidadService } from 'src/app/reutilizable/utilidad.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+  ocultarPassword: boolean = true;
+  mostrarLoading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private usuarioServicio: UsuarioService,
+    private utilidadServicio: UtilidadService
+  ) {
+    this.formularioLogin = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  iniciarSesion() {
+    this.mostrarLoading = true;
+
+    const request: Login = {
+      correo: this.formularioLogin.value.email,
+      clave: this.formularioLogin.value.password
+    }
+
+    this.usuarioServicio.iniciarSesion(request).subscribe({
+      next: (response) => {
+        if (response.status) {
+          this.utilidadServicio.guardarSesionUsuario(response.data);
+          this.router.navigate(['/pages']);
+        } else {
+          this.utilidadServicio.mostrarAlerta("No se encontraron coincidencias", "Opps!")
+        }
+      },
+      complete: () => {
+        this.mostrarLoading = false;
+      },
+      error: () => {
+        this.utilidadServicio.mostrarAlerta("Hubo un error", "Opps!")
+      }
+    });
+  }
 }

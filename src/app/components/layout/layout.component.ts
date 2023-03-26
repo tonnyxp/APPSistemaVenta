e3a09ae9677ca55bdaa2fc749278bbdbe25fe15b
Antonio Alvarado <tonny.xp@hotmail.com>
2023-03-26 16:23:25 -0600

    Implementación del menu dinámico

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { Menu } from 'src/app/interfaces/menu';
+
+import { MenuService } from 'src/app/services/menu.service';
+import { UtilidadService } from 'src/app/reutilizable/utilidad.service';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  listaMenus: Menu[] = [];
+  correoUsuario: string = '';
+  rolUsuario: string = '';
+
+  constructor(
+    private router: Router,
+    private menuServicio: MenuService,
+    private utilidadServicio: UtilidadService
+  ) { }
 
   ngOnInit(): void {
+
+    const usuario = this.utilidadServicio.obtenerSesionUsuario();
+
+    if (usuario != null) {
+      this.correoUsuario = usuario.correo;
+      this.rolUsuario = usuario.rolDescripcion;
+
+      this.menuServicio.lista(usuario.usuarioId).subscribe({
+        next: (response) => {
+          if (response.status) {
+            this.listaMenus = response.data;
+          }
+        },
+        error: () => { }
+      });
+    }
+  }
+
+  cerrarSesion() {
+    this.utilidadServicio.eliminarSesionUsuario();
+    this.router.navigate(['/login']);
   }
 
 }
